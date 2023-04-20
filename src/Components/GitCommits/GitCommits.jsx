460d1e532557ab21da385a20953c6358a8f67219
Deep Hansda <deephansda921@gmail.com>
2023-04-20 10:47:39 +0530

    git image working

--- src/Components/GitCommits/GitCommits.jsx
+++ src/Components/GitCommits/GitCommits.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import styles from './GitCommits.module.css'
-import Image from 'next/image'
+import React from "react";
+import styles from "./GitCommits.module.css";
+import Image from "next/image";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
 function GitCommits() {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "manjiro",
+    },
+  });
+  const img = cld.image("contributions.pngdeep");
+
   return (
     <div className={styles.gitContainer}>
-        <div className={styles.gitTitle}>
-          <h2>Git Commits.</h2>
+      <div className={styles.gitTitle}>
+        <h2>Git Commits.</h2>
+      </div>
+      <div className={styles.gitCommitsBg}>
+        <div className={styles.gitImageContainer}>
+          <AdvancedImage cldImg={img} />
         </div>
-        <div className={styles.gitCommitsBg}>
-            <div className={styles.gitImageContainer}>
-
-            <Image src={"https://res.cloudinary.com/manjiro/image/upload/v1681529705/portfolio_images/git/contributions_1_eimhgn.png"} alt='contributions' width={776} height={714}/>
-            </div>
-        </div>
-        <input type="date" onChange={(e)=>console.log(e.target.value)}/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GitCommits
+export default GitCommits;
