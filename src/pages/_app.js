4034512c6fdacb212cd31389747350827c195880
Deep Hansda <deephansda921@gmail.com>
2023-04-21 12:49:30 +0530

    analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import AppContextProvider from "@/Context/AppContext";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <AppContextProvider>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
+      <Analytics />
     </AppContextProvider>
   );
 }
